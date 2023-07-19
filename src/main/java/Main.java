ae875b63282b6c27a58c6e31e65e9f90edc6dd91
Adam Sajewicz <adam.saj@wp.pl>
2023-07-19 13:59:54 +0200

    Added more robust logging , and introduced AssertJ assertions.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import com.google.gson.Gson;
+import lombok.extern.slf4j.Slf4j;
 import model.Booking;
 import model.BookingDates;
 import model.BookingResponse;
@@ -9,6 +10,10 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
 public class Main {
     public static void main(String[] args) throws Exception {
 
@@ -34,10 +39,15 @@ public class Main {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(httpResponse.body());
+        assertThat(httpResponse.statusCode())
+                .withFailMessage("StatusCode is incorrect!")
+                .isEqualTo(200);
+        log.info("Successfully verified that the StatusCode is equal to: " + 200);
+        
+        log.info(httpResponse.body());
 
         BookingResponse bookingResponse = gson.fromJson(httpResponse.body(), BookingResponse.class);
 
-        System.out.println(bookingResponse.getBooking().getAdditionalneeds());
+        log.info("Just to double-check, the recorded booking's additional needs are: " + bookingResponse.getBooking().getAdditionalneeds());
     }
 }
